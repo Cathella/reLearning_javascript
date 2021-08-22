3147657e8b8305052e3c59b9a3a2f57c4f4f192f
Cathella <ncathie214@gmail.com>
2021-08-22 15:46:09 +0300

    while loop counter

--- 101.js
+++ 101.js
@@ -1,12 +1,6 @@
-const myNum = 5
+let counter = 0;
 
-// if (3) => true or if ("hello") => true
-// if (-1) => true or if (" ") => true but
-// if (0) => false or if ("") => false
-// if ({}) and if ([]) => true
-
-if(myNum > 3) {
-  console.log("it is true");
-} else {
-  console.log("it is false")
+while(counter < 10) {
+  console.log(counter)
+  counter += 1
 }
