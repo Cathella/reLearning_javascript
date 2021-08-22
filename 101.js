988b3d818422a85bcc036844b6045d1e6a1fe82f
Cathella <ncathie214@gmail.com>
2021-08-22 16:00:55 +0300

    arrays

--- 101.js
+++ 101.js
@@ -1,3 +1,13 @@
-for (let counter = 0; counter < 10; counter += 1) {
-  console.log(counter)
-}
+// think of an array as luggage, 
+// you can add any type of cloth to it
+const myArray = [5, "cheese", true]
+
+// add item to an array
+// myArray.push(5)
+
+console.log(myArray)
+
+// access individual array elements.
+console.log(myArray[0])
+console.log(myArray[1])
+console.log(myArray[2])
