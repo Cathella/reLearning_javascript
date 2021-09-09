ac34bce6cd473f48a4fcd90237ac71e773631122
Cathella <ncathie214@gmail.com>
2021-09-10 00:44:22 +0300

    Modify an Array Stored in an Object

--- recap.js
+++ recap.js
@@ -67,3 +67,26 @@ let userActivity = {
 
 userActivity.data.online = 45;
 
+// Modify an Array Stored in an Object
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: ['Sam', 'Kira', 'Tomo'],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+}
+
+addFriend(user, "Catherine");
