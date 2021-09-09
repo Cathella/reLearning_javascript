f97a03bac485ff0f9e44186275aea4da69e3223f
Cathella <ncathie214@gmail.com>
2021-09-09 14:29:39 +0300

    Access an array's contents using bracket notation

--- recap.js
+++ recap.js
@@ -1,3 +1,6 @@
 // Use an array to store a collection of data
 let yourArray = ["catherine", 1, "hello", 100, true];
 
+// Access an array's contents using bracket notation
+yourArray[1] = "is now a string";
+
