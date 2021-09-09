133cce5313562989b22b1ffbf8b14122efcd1d5c
Cathella <ncathie214@gmail.com>
2021-09-09 15:54:37 +0300

    Add items to an array with push() and unshift()

--- recap.js
+++ recap.js
@@ -4,3 +4,6 @@ let yourArray = ["catherine", 1, "hello", 100, true];
 // Access an array's contents using bracket notation
 yourArray[1] = "is now a string";
 
+// Add items to an array with push() and unshift()
+yourArray.push(23); // adds elements to the end of an array
+yourArray.unshift("hello"); // adds elements to the beginning.
