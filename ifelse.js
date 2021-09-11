50b130efd028c8112f8447a4403619e9675549e2
Cathella <ncathie214@gmail.com>
2021-09-11 18:34:19 +0300

    if else statements js

---
+++ ifelse.js
@@ -0,0 +1,52 @@
+// Introducing Else Statements
+function testElse(val) {
+  var result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+  return result;
+}
+testElse(4);
+
+// Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+
+// Logical Order in If Else Statements
+function orderMyLogic(val) {
+  if (val < 5 ) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
+
+// Chaining If Else Statements
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny"
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+testSize(7);
+
