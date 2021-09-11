61d2452450dd98d34830877e02a3c1b2ce7ea3ec
Cathella <ncathie214@gmail.com>
2021-09-11 04:23:25 +0300

    js basics

---
+++ basic.js
@@ -0,0 +1,102 @@
+// Javascript datatypes
+// undefined, null, symbol, number, bigint, object, string, boolean
+
+// Declare JavaScript Variables
+var myName;
+
+// Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Understanding Uninitialized Variables
+var a = 5; // When JavaScript variables are declared,
+var b = 10; //  they have an initial value of undefined
+var c = "I am a";
+
+a = a + 1; // If you do a mathematical operation on an undefined 
+b = b + 5; // variable your result will be NaN - "Not a Number"
+c = c + " String!"; // If you concatenate a string with an undefined variable, 
+// you will get a literal string of undefined
+
+// Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver; // Write variable names in JavaScript in camelCase
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Add Two Numbers with JavaScript
+var sum = 10 + 10;
+
+// Subtract One Number from Another with JavaScript
+var difference = 45 - 33;
+
+// Multiply Two Numbers with JavaScript
+var product = 8 * 10;
+
+// Divide One Number by Another with JavaScript
+var quotient = 66 / 33;
+
+// Increment a Number with JavaScript
+var myVar = 87;
+myVar++;
+
+// Decrement a Number with JavaScriptPassed
+var myVar = 11;
+myVar--;
+
+// Create Decimal Numbers with JavaScript
+var myDecimal = 5.8;
+
+// Multiply Two Decimals with JavaScript
+var product = 2.0 * 2.5;
+
+// Divide One Decimal by Another with JavaScript
+var quotient = 4.4 / 2.0;
+
+// Finding a Remainder in JavaScript
+var remainder = 11 % 3;
+
+// Compound Assignment With Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+
+// Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+
+// Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+
+// Compound Assignment With Augmented Division
+var a = 48;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+
--- objects.js
+++ objects.js
@@ -36,3 +36,87 @@ function checkInventory(scannedItem) {
 }
 console.log(checkInventory("apples"));
 
+// Use the delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+console.log(foods);
+
+// Check if an Object has a Property
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+// users.hasOwnProperty('Alan');
+// 'Alan' in users; - in keyword
+
+function isEveryoneHere(userObj) {
+  if (userObj.hasOwnProperty('Alan') && userObj.hasOwnProperty('Sarah') && userObj.hasOwnProperty('Ryan') && userObj.hasOwnProperty('Jeff')) {
+    return true;
+  }
+  return false;
+}
+console.log(isEveryoneHere(users));
+
+// Iterate Through the Keys of an Object with a for...in Statement
+function countOnline(usersObj) {
+  let result = 0;
+
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+}
+
+// Generate an Array of All Object Keys with Object.keys()
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
+}
+console.log(getArrayOfUsers(users));
+
+
