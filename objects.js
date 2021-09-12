0a0bcbd6756e78804aa2185a743246c0f5563c27
Cathella <ncathie214@gmail.com>
2021-09-12 09:27:20 +0300

    Add New Properties to a JavaScript Object

--- objects.js
+++ objects.js
@@ -162,3 +162,24 @@ var myDog = {
   "friends": ["freeCodeCamp Campers"]
 };
 myDog.name = "Happy Coder";
+
+// Add New Properties to a JavaScript Object
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = "woof woof";
+
+// Delete Properties from a JavaScript Object
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails;
+
+// 
