181a80ee8da8f9b0c00754d9e43e354f67c0d245
Cathella <ncathie214@gmail.com>
2021-09-12 09:42:28 +0300

    Using Objects for Lookups

--- objects.js
+++ objects.js
@@ -182,4 +182,18 @@ var myDog = {
 };
 delete myDog.tails;
 
-// 
+// Using Objects for Lookups
+function phoneticLookup(val) {
+  var result = "";
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank"
+  }
+  result = lookup[val];
+  return result;
+}
+phoneticLookup("charlie");
