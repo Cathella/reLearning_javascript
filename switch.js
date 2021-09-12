125963b88e52543d1eb4b2714781730b14f612cf
Cathella <ncathie214@gmail.com>
2021-09-12 06:37:07 +0300

    Multiple Identical Options in a Switch

--- switch.js
+++ switch.js
@@ -39,3 +39,27 @@ function switchOfStuff(val) {
   return answer;
 }
 switchOfStuff(1);
+
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+sequentialSizes(1);
