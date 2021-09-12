145d328e393335b7ca041b6d3928fe75cacaa4c5
Cathella <ncathie214@gmail.com>
2021-09-12 17:09:03 +0300

    Accessing Nested Objects

--- objects.js
+++ objects.js
@@ -227,3 +227,18 @@ var myMusic = [
   }
 ];
 
+// Accessing Nested Objects
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+var gloveBoxContents = myStorage.car.inside["glove box"];
+
+// 
