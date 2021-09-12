58976e4f3bf38334e52c7371bc4d8250691c9bdf
Cathella <ncathie214@gmail.com>
2021-09-12 08:14:30 +0300

    Accessing Object Properties with Bracket Notation

--- objects.js
+++ objects.js
@@ -135,3 +135,12 @@ var testObj = {
 };
 var hatValue = testObj.hat;
 var shirtValue = testObj.shirt;
+
+// Accessing Object Properties with Bracket Notation
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
