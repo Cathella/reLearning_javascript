e56966e6a3ffaf074c7f34e88f420668d95d3ea8
Cathella <ncathie214@gmail.com>
2021-09-12 06:46:12 +0300

    Replacing If Else Chains with Switch

--- switch.js
+++ switch.js
@@ -63,3 +63,44 @@ function sequentialSizes(val) {
   return answer;
 }
 sequentialSizes(1);
+
+// Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  var answer = "";
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+chainToSwitch(7);
+// - will become
+function chainToSwitch(val) {
+  var answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+chainToSwitch(7);
