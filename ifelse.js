4314ddf0652a2a0ca31bd3d72793b89bcea0bbcf
Cathella <ncathie214@gmail.com>
2021-09-12 06:08:56 +0300

    switch statement js

--- ifelse.js
+++ ifelse.js
@@ -50,3 +50,23 @@ function testSize(num) {
 }
 testSize(7);
 
+// Golf Code
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+}
+golfScore(5, 4);
