ff01dd17b51faac1b9bed441e4dee2ab39f0bd3c
Cathella <ncathie214@gmail.com>
2021-09-12 08:11:28 +0300

    Accessing Object Properties with Dot Notation

--- objects.js
+++ objects.js
@@ -126,3 +126,12 @@ var myDog = {
   tails: 1,
   friends: ["ella", "ade"]
 };
+
+// Accessing Object Properties with Dot Notation
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
